546b01307c14b7925e0c58382f6c9fec7c86d3f4
deepak9199 <84835673+deepak9199@users.noreply.github.com>
2024-06-01 13:46:29 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,12 +5,6 @@ export const environment = {
 
 
  firebaseConfig : {
-  apiKey: "AIzaSyCzctI56ZUVu6Vx-RuTTzXi9j3a1kItF6M",
-  authDomain: "fancyzone-1afee.firebaseapp.com",
-  projectId: "fancyzone-1afee",
-  storageBucket: "fancyzone-1afee.appspot.com",
-  messagingSenderId: "173693620812",
-  appId: "1:173693620812:web:212c9dfe25e5a9dab19ae9",
-  measurementId: "G-XHSWCCF5KE"
+
   }
 }
